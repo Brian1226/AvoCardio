a4d081a02004f9a495dc8a500fbd72186000fe70
Brian Nguyen <nguyenbrian1226@gmail.com>
2023-03-11 12:46:01 -0800

    added template inheritance for HTML title tag

--- main.py
+++ main.py
@@ -7,30 +7,24 @@ app.config["SECRET_KEY"] = "mykey"
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    title = "Home"
+    return render_template("home.html", title=title)
 
 @app.route("/login")
 def login():
     form = loginForm()
     if form.validate_on_submit():
-        session["email"] = form.email.data
-        session["name"] = form.name.data
-        session["message"] = form.message.data
         return redirect(url_for("home"))
-    else:
-        flash("Invalid email or password")
     return render_template("login.html", form=form)
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
+    title = "Contact"
     form = contactForm()
     if form.validate_on_submit():
         flash("THANKS FOR CONTACTING US!")
-        session["email"] = form.email.data
-        session["name"] = form.name.data
-        session["message"] = form.message.data
         return redirect(url_for("contact"))
-    return render_template("contact.html", form=form)
+    return render_template("contact.html", form=form, title=title)
 
 if __name__ == "__main__":
     app.run(debug=True)
