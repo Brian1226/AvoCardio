75162b9cf7cb8888a75568a27092460a448fbbc3
Brian Nguyen <nguyenbrian1226@gmail.com>
2023-03-11 12:10:12 -0800

    added login page

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import StringField, PasswordField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Email
 
 class contactForm(FlaskForm):
@@ -7,3 +7,9 @@ class contactForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()], render_kw={"placeholder": "Enter your name"})
     message = TextAreaField("Message", validators=[DataRequired()], render_kw={"placeholder": "Enter your message", "rows" : 8})
     submit = SubmitField("Submit")
+
+class loginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()], render_kw={"placeholder": "Enter your email", "type": "email"})
+    password = PasswordField("Password", validators=[DataRequired()], render_kw={"placeholder": "Enter your password"})
+    login = SubmitField("Log In")
+    signup = SubmitField("Sign Up")
