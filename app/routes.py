e5c4be55436a5d696bfb8230732f99b4f7c11142
Brian Nguyen <nguyenbrian1226@gmail.com>
2023-12-17 15:03:50 -0800

    increased recipes result to 40

--- app/routes.py
+++ app/routes.py
@@ -112,7 +112,7 @@ def search_recipes(query):
     params = {
         'apiKey': SPOONACULAR_API_KEY,
         'query': query,
-        'number': 20,
+        'number': 40,
         'instructionsRequired': True,
         'addRecipeInformation': True,
         'fillIngredients': True,
