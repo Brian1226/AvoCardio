169c3722626e5fbb6bb4a7f8e642b21611fa4451
Brian Nguyen <nguyenbrian1226@gmail.com>
2023-12-17 14:01:43 -0800

    secrets

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_mail import Mail
+from dotenv import load_dotenv
 
-path = os.path.abspath(os.getcwd()+"/app/database/database.db")
+load_dotenv()
+
+# path = os.path.abspath(os.getcwd()+"/app/database/database.db")
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + path
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + path
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.getcwd() + os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
